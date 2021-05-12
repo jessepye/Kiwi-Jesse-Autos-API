f257b01c91505c508a95c76031d7a032d4ad03f3
Kiwi Bui <kiwibui@kiwis-mbp.lan>
2021-05-12 11:01:31 -0700

    Stubbed AutosServiceTest

---
+++ src/test/java/com/galvanize/autos/AutosServiceTest.java
@@ -0,0 +1,32 @@
+package com.galvanize.autos;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutosServiceTest {
+
+    @Test
+    void getAutos() {
+    }
+
+    @Test
+    void testGetAutos() {
+    }
+
+    @Test
+    void addAuto() {
+    }
+
+    @Test
+    void getAuto() {
+    }
+
+    @Test
+    void updateAuto() {
+    }
+
+    @Test
+    void deleteAuto() {
+    }
+}
